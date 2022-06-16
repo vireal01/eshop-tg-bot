91636174c169d7202aa5ce09671287c6965b6d17
vireal01 <vireal014@yandex.ru>
2022-06-17 02:13:31 +0400

    removed data file from repo

---
+++ src/scripts/refreshGamesList.js
@@ -0,0 +1,6 @@
+import cron from 'node-cron';
+import Api from '../services/eshopApiRequests.js';
+
+cron.schedule('*/6 * * * * ', async function() {
+    await Api.getGamesOfEuropeRegion()
+  });
--- src/services/eshopApiRequests.js
+++ src/services/eshopApiRequests.js
@@ -1,11 +1,13 @@
 import * as nintendo from 'nintendo-switch-eshop';
 import * as fs from 'fs';
-import data from '../models/euGamesList.json' assert {type: "json"}
+// import data from '../models/euGamesList.json' assert {type: "json"}
 
 export default class Api {
+    static dataFilePath = './src/models/euGamesList.json'
+
     static async getGamesOfEuropeRegion() {
         const ans = await nintendo.getGamesEurope()
-        fs.writeFile('./src/models/euGamesList.json', JSON.stringify(ans), err => {
+        fs.writeFile(this.dataFilePath, JSON.stringify(ans), err => {
             if (err) {
                 console.error(err);
             } else {
@@ -14,26 +16,34 @@ export default class Api {
         });
     }
 
-    static getGameObjByUrl(url) {
+    static async checkDataFileExists() {
+        if (!fs.existsSync(this.dataFilePath) || fs.read(this.dataFilePath).length === 0) {
+            await Api.getGamesOfEuropeRegion();
+        }
+        return fs.readFileSync(this.dataFilePath)
+    }
+
+    static async getGameObjByUrl(url) {
         const regex = /\/Games.*/g
         const parsedUrl = url.match(regex)[0]
         let gameData
+        const data = await this.checkDataFileExists();
         data.forEach(element => {
-            if(element.url === parsedUrl){
+            if (element.url === parsedUrl) {
                 return gameData = element
             }
-        });
+        })
         return gameData
     }
 
-    static async getGamePrice({country = 'PL', gameId = '70010000034009'}) {
+    static async getGamePrice({ country = 'PL', gameId = '70010000034009' }) {
         const ans = await nintendo.getPrices(country, gameId)
         // console.log(JSON.stringify(ans.prices[0])) 
         return ans.prices[0]
     }
 }
 // Api.getGamePrice({})
-Api.getGamesOfEuropeRegion()
+// Api.getGamesOfEuropeRegion()
 // console.log(data[0].fs_id)
 // console.log(Api.getGameObjByUrl('https://www.nintendo.co.uk/Games/Nintendo-Switch-download-software/Moonlighter-1423773.html'))
 // module.exports = Api; 
