1244ad77939870e438277ce830e118dbc93e653e
vireal01 <vireal014@yandex.ru>
2022-06-17 02:52:39 +0400

    possible fix

--- src/services/eshopApiRequests.js
+++ src/services/eshopApiRequests.js
@@ -20,7 +20,7 @@ export default class Api {
         if (!fs.existsSync(this.dataFilePath) || fs.readFileSync(this.dataFilePath).length === 0) {
             await Api.getGamesOfEuropeRegion();
         }
-        return JSON.stringify(fs.readFileSync(this.dataFilePath))
+        return JSON.parse(fs.readFileSync(this.dataFilePath))
     }
 
     static async getGameObjByUrl(url) {
