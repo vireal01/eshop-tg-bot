a70cbf78bb8fad76d92665e2813aa0d3c607b73b
vireal01 <vireal014@yandex.ru>
2022-06-17 04:56:37 +0400

    Added abulity to add region

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { Telegraf } from 'telegraf';
+import { Telegraf, Markup } from 'telegraf';
 import GameList from './services/gamelist.js';
 import GameInfo from './services/gameInfo.js';
 import dotenv from "dotenv";
@@ -6,7 +6,7 @@ dotenv.config({ silent: process.env.NODE_ENV === 'production' });
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.start(async (ctx) => {
-    ctx.reply('/list - show all added games\n/addGame ${gametitle} tio add gama to the list')
+    ctx.reply('/keyboard - spawn keyboard \n/list - show all added games\n/addGame ${gametitle} tio add gama to the list')
 })
 bot.help((ctx) => ctx.reply('Send me a sticker'))
 // bot.on('sticker', (ctx) => ctx.reply('👍'))
@@ -27,6 +27,63 @@ bot.hears(/\/find (.+)/, async (ctx) => {
     const prices = await GameInfo.getPrices(url)
     const stringifiedData = await GameInfo.stringifyPriceData(prices)
     ctx.reply(stringifiedData.join('\n'))
+})
+
+// bot.command('keyboard', (ctx) =>
+//     ctx.reply('One time keyboard', Markup
+//         .keyboard(['/simple', '/inline', '/pyramid'])
+//         // .oneTime()
+//         // .resize()
+//     )
+// )
+
+bot.command('keyboard', async (ctx) => {
+    return await ctx.reply('Custom buttons keyboard', Markup
+        .keyboard([
+            ['🇬🇧 Show regions', 'Add region']
+        ])
+        // .oneTime()
+        .resize()
+    )
+})
+
+bot.hears('🇬🇧 Show regions', async (ctx) => {
+    let contriesText = '';
+    for (let countryCode of GameInfo.regionList) {
+        contriesText += `${GameInfo.getCountyFlagByCode(countryCode)} ${countryCode}\n`
+    }
+    ctx.reply(contriesText)
+})
+
+bot.hears('Add region', async (ctx) => {
+    const availableRegions = []
+    for (let countryCode of GameInfo.availableRegionsList) {
+        availableRegions.push(`/addRegion ${GameInfo.getCountyFlagByCode(countryCode)}`)
+    }
+    availableRegions.push('◀️')
+    return await ctx.reply('Choose a region to add', Markup
+        .keyboard([
+            availableRegions
+        ])
+        // .oneTime()
+        .resize()
+    )
+})
+
+bot.hears('◀️', async (ctx) => {
+    return await ctx.reply('/keyboard', Markup
+        .keyboard([
+            ['🇬🇧 Show regions', 'Add region']
+        ])
+        // .oneTime()
+        .resize()
+    )
+})
+
+bot.hears(/\/addRegion (.+)/, async (ctx) => {
+    const countryFlag = ctx.match[1]
+    const countryCode = GameInfo.getCountyCodeByFlag(countryFlag)
+    GameInfo.updateRegionList(countryCode)
 })
 
 bot.launch()
